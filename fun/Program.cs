b7aa1f09347ac2a06d098de90ae6d1adb5b1ce4a
Joachim <Joachim230@gmail.com>
2024-04-08 11:31:52 +0200

    LOL

---
+++ fun/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Joachim is the ultimate githubber2k");
+    }
+}
