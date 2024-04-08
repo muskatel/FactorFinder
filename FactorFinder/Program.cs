aeecf944adb96adc833c7b74f270ba0f5874200a
Craig Marais <craig.marais.85@gmail.com>
2024-04-08 12:22:14 +0200

    best code 2024

--- FactorFinder/Program.cs
+++ FactorFinder/Program.cs
@@ -1,22 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int num = 42;
+int num = 733;
 
-Console.WriteLine($"Given {num}");
+Console.WriteLine($"Given {num}\n factors are:");
 
 foreach (int i in Finder(num))
 {
-    Console.WriteLine($"{i} is a factor");
+    Console.WriteLine($"{i}");
 }
 
 // returns prime factors of a number
 // IE   8   -> 2 x 2 x 2
 //      14  -> 2 x 7
-// num will always be less than 1000
 List<int> Finder(int num)
 {
-    
     List<int> factors = new List<int>();
-
+    List<int> usedFactors = new List<int>();
+    for (int i = 2; i <= num; i++)
+    {
+        foreach (int usedFactor in usedFactors)
+        {
+            if (i % usedFactor == 0)
+            {
+                //divisible by a previous number
+                goto skip;
+            }
+        }
+        if (num % i == 0)
+        {
+            factors.Add(i);
+            
+            usedFactors.Add(i);
+        }
+        skip: ;
+    }
 
     return factors;
 }
